81ace71fbcb94211d750e1cbfaad44a6e737e851
Samvel <samsam2004@yandex.ru>
2023-09-16 00:38:17 +0300

    123

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from .views import *
 urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),  # int pk- id нашего поста
     path('about/', AboutPageView.as_view(), name='about'),  # информация о том как открывается наша страничка
-    path('', Bloglist.as_view(), name='home'),
+    path('', Bloglist.as_view(), name='index'),
     path('search/', SearchResultsView.as_view(), name='search'),
     path('pozn/', PoznavatelniyCategory.as_view(), name='pozn'),
     path('len/', LeniviyCategory.as_view(), name='len'),
